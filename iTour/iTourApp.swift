085838becc75e87e4d9c7f1e41a60ef4a76af73e
Vincent Senjaya <73159146+vincentsenjaya24@users.noreply.github.com>
2024-06-19 22:04:33 +0700

    Initial Commit

---
+++ iTour/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by Vincent Senjaya on 19/06/24.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
